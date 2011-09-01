5b3e211f6158453f113e6fbb11a49d0ea0ecb6a3
Igor Davydov <iskiche@gmail.com>
2011-09-01 16:46:36 +0400

    semicolon

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,12 @@
 class AlbumsController < ApplicationController
+  
   def index
     @albums = Album.order("position")
   end
 
   def show
    # @albums = Album.published # for body_content_right
-    @album = Album.find(params[:id]
+    @album = Album.find(params[:id])
   end
   
 
