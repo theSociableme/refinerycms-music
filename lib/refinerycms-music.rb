3ddccbfe8f6f42757dcaff64fc15863c1a9911d3
Igor Davydov <iskiche@gmail.com>
2011-09-02 17:59:01 +0400

    fff

--- lib/refinerycms-music.rb
+++ lib/refinerycms-music.rb
@@ -10,12 +10,14 @@ module Refinery
       config.after_initialize do
         Refinery::Plugin.register do |plugin|
           plugin.name = "music"
-          plugin.menu_match = /(admin|refinery)\/(albums|songs|music_settings)?$/
-          plugin.url = {:controller => '/admin/albums', :action => 'index'}
-          plugin.activity = {
-            :class => Album,
-            :title => 'title'
-          }
+   #       plugin.menu_match = /(admin|refinery)\/(albums|songs|music_settings)?$/
+   
+          plugin.menu_match = /(admin|refinery)\/(albums|songs|music_settings)$/
+          #plugin.url = {:controller => '/admin/albums', :action => 'index'}
+          #plugin.activity = {
+          #  :class => Album,
+          #  :title => 'title'
+          #}
         end
       end
     end
