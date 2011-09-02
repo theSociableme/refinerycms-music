8ab5ead1184ce6818af269b0e84648f6f72fff5d
Igor Davydov <iskiche@gmail.com>
2011-09-03 01:22:15 +0400

    re create for song

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -1,7 +1,15 @@
 module Admin
   class SongsController < Admin::BaseController
     crudify :song, :title_attribute => :title
-    
+    def create
+      @song = Song.new(params(:song))
+      if @song.save
+        redirect_to admin_album_songs_path
+        flash[:notice] = 'Song created'
+      else
+        render :action => "new"
+      end
+    end
 
   end
 end
