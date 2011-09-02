44e3f825ea511915912707d9987b817d320e9c30
Igor Davydov <iskiche@gmail.com>
2011-09-03 01:40:18 +0400

    redirect_to admin_album_path(@album)

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -4,7 +4,7 @@ module Admin
     def create
       @song = Song.new(params[:song])
       if @song.save
-        redirect_to admin_album_songs_path
+        redirect_to admin_album_path(@album)
         flash[:notice] = 'Song created'
       else
         render :action => "new"
