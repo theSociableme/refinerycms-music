6b70d5bcc13af0ac6035d46b026ae57ecf5917ca
Igor Davydov <iskiche@gmail.com>
2011-10-06 01:55:41 +0400

    Different way

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,9 +1,8 @@
 class Album < ActiveRecord::Base
-  
+ 
+  belongs_to :title_image, :class_name => 'Image'
   has_many :songs
   accepts_nested_attributes_for :songs
   validates_presence_of :title
-  has_one :image
-  accepts_nested_attributes_for :image, :allow_destroy => false
- 
+  
 end
