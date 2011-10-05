01f5b5dda7fe696456c26170e1954646731fbafc
Igor Davydov <iskiche@gmail.com>
2011-10-06 02:00:15 +0400

    Fix migration

--- db/migrate/3_add_image_to_album.rb
+++ db/migrate/3_add_image_to_album.rb
@@ -2,12 +2,12 @@ class AddImageIdToAlbums < ActiveRecord::Migration
 
   def self.up
     unless ::Album.column_names.map(&:to_sym).include?(:image_id)
-      add_column ::Album.table_name, :image_id, :integer
+      add_column ::Album.table_name, :title_image_id, :integer
     end
   end
 
   def self.down
-    remove_column ::Album.table_name, :image_id
+    remove_column ::Album.table_name, :title_image_id
   end
 
 end
