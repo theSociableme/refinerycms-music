61e0c51e62b0163217d7fca298c6156f46a8370a
greendog <alexander.negoda@gmail.com>
2011-11-10 19:01:10 +0400

    fix

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,15 @@
+class SongsController < ::ApplicationController
+  before_filter :find_page
+
+  def show
+    # @album = Album.find(params[:album_id])
+    @song = Refinery::Song.find(params[:id])
+    @album = Refinery::Album.find(params[:album_id])
+  end
+
+  protected
+
+  def find_page
+    @page = Refinery::Page.find_by_link_url("/music")
+  end
+end
